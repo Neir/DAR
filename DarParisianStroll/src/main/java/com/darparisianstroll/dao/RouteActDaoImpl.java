904fadf53fce01eacd7decde1eaaa858e933c55d
AFFES Mohamed <mohamed.af@hotmail.fr>
2015-11-11 02:32:41 +0100

    correction dao

--- DarParisianStroll/src/main/java/com/darparisianstroll/dao/RouteActDaoImpl.java
+++ DarParisianStroll/src/main/java/com/darparisianstroll/dao/RouteActDaoImpl.java
@@ -1,5 +1,6 @@
 package com.darparisianstroll.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
@@ -40,15 +41,18 @@ public class RouteActDaoImpl extends AbstractDao<Integer, RouteAct> implements
     public List<Activity> findAllActivitiesByRoute(Route r) {
 	Criteria c = createEntityCriteria();
 	c.add(Restrictions.eq("route", r.getRoute_id()));
-
+	List<RouteAct> routeAct = c.list();
 	Disjunction d = Restrictions.disjunction();
-	for (RouteAct ra : (List<RouteAct>) c.list()) {
+	for (RouteAct ra : routeAct) {
 	    d.add(Restrictions.eq("id_activity", ra.getActivity()));
 	}
-	Criteria c2 = getSession().createCriteria(Activity.class);
-	c2.add(d);
-
-	return c2.list();
+	List<Activity> la = new ArrayList<>(0);
+	if (routeAct.size() > 0) {
+	    Criteria c2 = getSession().createCriteria(Activity.class);
+	    c2.add(d);
+	    la.addAll(c2.list());
+	}
+	return la;
     }
 
     @SuppressWarnings("unchecked")
@@ -56,15 +60,19 @@ public class RouteActDaoImpl extends AbstractDao<Integer, RouteAct> implements
     public List<Route> findAllRoutesByActivities(Activity a) {
 	Criteria c = createEntityCriteria();
 	c.add(Restrictions.eq("activity", a.getId_activity()));
+	List<RouteAct> routeAct = c.list();
 
 	Disjunction d = Restrictions.disjunction();
-	for (RouteAct ra : (List<RouteAct>) c.list()) {
+	for (RouteAct ra : routeAct) {
 	    d.add(Restrictions.eq("route_id", ra.getRoute()));
 	}
-	Criteria c2 = getSession().createCriteria(Route.class);
-	c2.add(d);
-
-	return c2.list();
+	List<Route> lr = new ArrayList<Route>(0);
+	if (routeAct.size() > 0) {
+	    Criteria c2 = getSession().createCriteria(Route.class);
+	    c2.add(d);
+	    lr.addAll(c2.list());
+	}
+	return lr;
     }
 
     @SuppressWarnings("unchecked")
